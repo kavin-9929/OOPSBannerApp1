afc6a7c58661aa6c99f2f985b2615c80c6ba71e2
kavin CU <ku9929@srmist.edu.in>
2026-02-23 08:39:35 +0530

    [KAVIN C U]added:UC3

---
+++ uc3.java
@@ -0,0 +1,56 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        // Each row of the banner (7 rows total)
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                "  ****** ",
+                "  *****  "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "       * "
+        ));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *     * "
+        ));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  *****  "
+        ));
+    }
+}
